1ae044d6005464e040a33b0004584b306e1fe750
Thibeault De Mulder <thibeaultdm@hotmail.com>
2024-06-11 18:19:40 +0200

    string.Empty -> to prevent cannont insert null excetion

--- Thibeault.EindWerk.Objects/Customer.cs
+++ Thibeault.EindWerk.Objects/Customer.cs
@@ -8,9 +8,9 @@ namespace Thibeault.EindWerk.Objects
         [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
 
-        public string TrackingNumber { get; set; } 
-        public string FullName { get; set; } 
-        public string Email { get; set; }
+        public string TrackingNumber { get; set; }
+        public string FullName { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
         public Address Address { get; set; } = new();
         public List<OrderHeader>? Orders { get; set; } = new();
     }
