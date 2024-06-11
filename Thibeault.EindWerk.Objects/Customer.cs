cf0c272eaba940c36c4a31189db5fa60b8c548f5
Thibeault De Mulder <thibeaultdm@hotmail.com>
2024-06-11 18:21:12 +0200

    commented

--- Thibeault.EindWerk.Objects/Customer.cs
+++ Thibeault.EindWerk.Objects/Customer.cs
@@ -9,9 +9,9 @@ namespace Thibeault.EindWerk.Objects
         public int Id { get; set; }
 
         public string TrackingNumber { get; set; }
-        public string FullName { get; set; } = string.Empty;
+        public string FullName { get; set; } = string.Empty; // To prevent cannot save null exception on creation
         public string Email { get; set; } = string.Empty;
         public Address Address { get; set; } = new();
-        public List<OrderHeader>? Orders { get; set; } = new();
+        public List<OrderHeader>? Orders { get; set; } = new(); // So I can do .Add imeditly
     }
 }
